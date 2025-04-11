16f05bb9cdd345110946bd4fe11797432ef148ee
Aamir Ahmad <aamir557ahmad@gmail.com>
2025-04-11 22:51:48 +0530

    React Task 1 added

--- Day-4 (Friday)/React/src/App.jsx
+++ Day-4 (Friday)/React/src/App.jsx
@@ -1,34 +1,44 @@
-import React, {useState, useEffect}from 'react';
-import axios from "axios"
-import CryptoList from "./Components/CryptoList"
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CryptoList from "./Components/CryptoList";
+import Pagination from "./Components/Pagination";
+import "./App.css";
 
 const App = () => {
-  const[coinData, setCoinData] = useState([]);
+  const [coinData, setCoinData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(8);
 
   useEffect(() => {
     const fetchCoin = async () => {
       try {
         const res = await axios.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
-         
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
         );
         setCoinData(res.data);
       } catch (error) {
         console.error("Error fetching data", error);
       }
     };
-  
-    fetchCoin(); 
+
+    fetchCoin();
   }, []);
 
+  const LastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = LastPostIndex - postPerPage;
+  const currentPost = coinData.slice(firstPostIndex, LastPostIndex);
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Crypto Currency</h1>
-      <CryptoList coinData={coinData} />
-      </div>
-  )
-}
+      <CryptoList coinData={currentPost} />
+      <Pagination 
+      totalPosts={coinData.length} 
+      postPerPage={postPerPage}
+      setCurrentPage ={setCurrentPage} 
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ Day-4 (Friday)/React/src/Components/Pagination.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Pagination.css';
+
+const Pagination = ({ totalPosts, postPerPage,setCurrentPage }) => {
+    let pages = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+        pages.push(i);
+    }
+
+    return (
+        <div className="pagination-container">
+            {
+                pages.map((page, index) => (
+                    <button key={index} onClick={()=> setCurrentPage(pages)} className="pagination-button">
+                        {page}
+                    </button>
+                ))
+            }
+        </div>
+    );
+};
+
+export default Pagination;
